9659ba3d51f3343318659f2d664b8b5f175eedb5
Sondre <sondrehjetland@gmail.com>
2015-02-24 12:56:47 +0100

    GetCalendars

--- src/queries/CalendarQueries.java
+++ src/queries/CalendarQueries.java
@@ -1,36 +1,37 @@
 package queries;
 
 import database.DBConnect;
+import models.Calendar;
+import models.Person;
 import models.UserGroup;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class CalendarQueries {
 
-    public static ArrayList<UserGroup> getUserGroup() throws SQLException {
-        ArrayList<UserGroup> users = new ArrayList<>();
+    public static ArrayList<UserGroup> getCalendars(UserGroup userGroup) throws SQLException {
+        ArrayList<Calendar> calendars = new ArrayList<>();
         Connection con = DBConnect.getConnection();
         //Execute query
-        Statement stmt = con.createStatement();
+        
         String sql = "SELECT * FROM UserGroup";
-        ResultSet rs = stmt.executeQuery(sql);
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery(sql);
 
         while (rs.next()) {
             int UserGroupID = rs.getInt("UserGroupID");
             System.out.println(UserGroupID);
         }
         rs.close();
-        stmt.close();
+        pstmt.close();
         con.close();
         return users;
     }
     
     public static void main (String[] args) throws SQLException {
-        getUserGroup();
+        ArrayList<Person> person =  new ArrayList<>();
+        getCalendars(new UserGroup(3,"Sondre", person));
         
     }
     
