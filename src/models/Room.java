5e6cab3f5519b45764a022c5b4a5e458f5cbbacc
Nikolai Hegelstad <nikolai@hegelstad.net>
2015-02-24 12:38:25 +0100

    cleaned up room.java

--- src/models/Room.java
+++ src/models/Room.java
@@ -1,26 +1,30 @@
 package models;
 
-public class Room {
-	private String roomName;
-	private int capasity;
-	
-	public Room(String roomName, int capasity) {
-		super();
-		this.roomName = roomName;
-		this.capasity = capasity;
-	}
-	public String getRoomName() {
-		return roomName;
-	}
-	public void setRoomName(String roomName) {
-		this.roomName = roomName;
-	}
-	public int getCapasity() {
-		return capasity;
-	}
-	public void setCapasity(int capasity) {
-		this.capasity = capasity;
-	}
-	
-	
+import java.io.Serializable;
+
+public class Room implements Serializable{
+    private String roomName;
+    private int capasity;
+
+    public Room(String roomName, int capasity) {
+        this.roomName = roomName;
+        this.capasity = capasity;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public int getCapasity() {
+        return capasity;
+    }
+
+    public void setCapasity(int capasity) {
+        this.capasity = capasity;
+    }
+    
 }
