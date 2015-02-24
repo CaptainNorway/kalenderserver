1b5173a2391a5729939d4d52e594305ac0019313
Nikolai Hegelstad <nikolai@hegelstad.net>
2015-02-24 12:37:23 +0100

    cleaned up booking.java

--- src/models/Booking.java
+++ src/models/Booking.java
@@ -1,23 +1,37 @@
 package models;
 
-public class Booking {
-	Room room;
-	Event event;
-	public Booking(Room room, Event event) {
-		super();
-		this.room = room;
-		this.event = event;
-	}
-	public Room getRoom() {
-		return room;
-	}
-	public void setRoom(Room room) {
-		this.room = room;
-	}
-	public Event getEvent() {
-		return event;
-	}
-	public void setEvent(Event event) {
-		this.event = event;
-	}
+import java.io.Serializable;
+
+public class Booking implements Serializable {
+    private Room room;
+    private Event event;
+
+    public Booking(Room room, Event event) {
+        this.room = room;
+        this.event = event;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking(" +
+                "room: " + room +
+                ", event: " + event +
+                ')' ;
+    }
 }
