610fd08cd89a77d515e70e4e9842ef21919e6ce0
Nikolai Hegelstad <nikolai@hegelstad.net>
2015-02-25 14:42:35 +0100

    fixed room model and queries

--- src/models/Room.java
+++ src/models/Room.java
@@ -3,13 +3,16 @@ package models;
 import java.io.Serializable;
 
 public class Room implements Serializable {
+    private int roomID;
     private String roomName;
     private int capacity;
 
-    public Room(String roomName, int capacity) {
+    public Room(int roomID, String roomName, int capacity) {
+        this.roomID = roomID;
         this.roomName = roomName;
         this.capacity = capacity;
     }
+    public int getRoomID() { return roomID; }
 
     public String getRoomName() {
         return roomName;
@@ -19,18 +22,19 @@ public class Room implements Serializable {
         this.roomName = roomName;
     }
 
-    public int getcapacity() {
+    public int getCapacity() {
         return capacity;
     }
 
-    public void setcapacity(int capacity) {
+    public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
 
     @Override
     public String toString() {
         return "Room(" +
-                "roomName: " + roomName +
+                "ID: " + roomID +
+                ", name: " + roomName +
                 ", capasity: " + capacity +
                 ')';
     }
