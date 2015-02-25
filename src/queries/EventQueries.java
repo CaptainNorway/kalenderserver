099d6137d1a3dc6452b13f000cf2818e5d00e741
CPekko <eirik_fosse2@hotmail.com>
2015-02-25 13:07:20 +0100

    Started on EventQueries, not complete yet.

--- src/queries/EventQueries.java
+++ src/queries/EventQueries.java
@@ -1,7 +1,12 @@
 package queries;
 
+import java.sql.BatchUpdateException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import database.DBConnect;
@@ -9,31 +14,67 @@ import models.Event;
 import models.Calendar;
 
 public class EventQueries {
+	
+	
+	public static void checkUpdateCounts(int[] updateCounts) {
+		for (int i = 0; i < updateCounts.length; i++) {
+			if (updateCounts[i] >= 0) {
+				System.out.println("Successfully executed; updateCount=" + updateCounts[i]);
+			} else if (updateCounts[i] == Statement.SUCCESS_NO_INFO) {
+				System.out.println("Successfully executed; updateCount=Statement.SUCCESS_NO_INFO");
+			} else if (updateCounts[i] == Statement.EXECUTE_FAILED) {
+				System.out.println("Failed to execute; updateCount=Statement.EXECUTE_FAILED");
+			}
+		}
+	}
 
 	public static ArrayList<Event> getEvents(ArrayList<Calendar> cal){
 		Connection conn = null;
 	    PreparedStatement pstmt = null;
+	    ArrayList<Event> events = new ArrayList<>();
 	    try {
 	      conn = DBConnect.getConnection();
 	      conn.setAutoCommit(false);
 	      String query = ""
-	      		+ "SELECT into add_batch_table(stringCol, intCol) values(?, ?)";
+	      		+ "SELECT * "
+	      		+ "FROM Calendar NATURAL JOIN CalendarEvent NATURAL JOIN Event "
+	      		+ "WHERE ";
+	      for (int i = 0; i< cal.size(); i++){
+	    	  if (i != 0){
+	    		  query += "OR ";
+	    	  }
+	    	  query = query + "CalendarID = '?' ";
+	      }
 	      pstmt = conn.prepareStatement(query);
-	      pstmt.setString(1, "1");
-	      pstmt.setInt(2, 100);
-	      pstmt.addBatch();
-
-	      pstmt.setString(1, "2");
-	      pstmt.setInt(2, 200);
-	      pstmt.addBatch();
-
-	      pstmt.setString(1, "3");
-	      pstmt.setInt(2, 300);
-	      pstmt.addBatch();
-
-	      int[] updateCounts = pstmt.executeBatch();
-	      checkUpdateCoun
-		
-		return null;
+	      for (int i = 0; i < cal.size(); i++){
+	    	  pstmt.setInt(i+1, cal.get(i).getCalendarID());
+	      }
+	      
+	      ResultSet result = pstmt.executeQuery(query);
+	      
+	      System.out.println("We found something");
+	      
+	      while (result.next()) {
+	    	  int eventID = result.getInt("EventID");
+	    	  int calendarID = result.getInt("CalendarID");
+	    	  String calendarName = result.getString("CalendarName");
+	    	  String eventName = result.getString("Name");
+	    	  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd hh:mm:ss.fffffffff");
+	    	  LocalDateTime from = LocalDateTime.parse(result.getTimestamp("From").toString(), formatter);
+	    	  LocalDateTime to = LocalDateTime.parse(result.getTimestamp("To").toString(), formatter);
+	    	  
+	    	  ArrayList<Calendar> templist = new ArrayList<>();
+	    	  templist.add(new Calendar(calendarID, null, null));
+	    	  
+	    	  // getUserGroup må lages!!!!!!!!!!
+	    	  Calendar calendar = new Calendar (calendarID, calendarName, getUserGroup(templist));
+	    	  
+	    	  events.add(new Event(eventID, eventName, null, from, to, calendar));
+	      }
+	      
+		return events;
+	  }catch (Exception e){
+		System.out.println(e);
+	  }
 	}
 }
