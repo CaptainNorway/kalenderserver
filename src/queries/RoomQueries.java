b68c63dff3b2893e626ea1ffd55299ed58132c98
CaptainNorway <oyvind.reinsve@outlook.com>
2015-02-26 12:00:03 +0100

    Fjernet RoomID på Room-modellen og gjorde ferdig RoomQueries, men maa testet

--- src/queries/RoomQueries.java
+++ src/queries/RoomQueries.java
@@ -1,7 +1,9 @@
 package queries;
 
 import database.DBConnect;
+import models.Event;
 import models.Room;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -20,25 +22,63 @@ public class RoomQueries {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                int RoomID = resultSet.getInt("RoomID");
                 String RoomName = resultSet.getString("RoomName");
                 int Capacity = resultSet.getInt("Capacity");
-                rooms.add(new Room(RoomID, RoomName, Capacity));
-            }
+                rooms.add(new Room(RoomName, Capacity));
+                }
             resultSet.close();
             preparedStatement.close();
             con.close();
             return rooms;
         } catch (SQLException e) {
-            throw new IllegalArgumentException(e);
+            System.out.println(e);
+            return null;
         }
     }
 
-    public static void main(String[] args) {
-        ArrayList<Room> rooms = RoomQueries.getRooms();
-        System.out.println("Printing a list of the room entries in the database table ROOM:");
-        for (Room room : rooms) {
-            System.out.println(room);
-        }
+    public static ArrayList<Room> getAvailableRooms(Event event){
+    	ArrayList<Room> availableRooms = new ArrayList();
+    	int eventID = event.getEventID();
+    	try{
+    		Connection con = DBConnect.getConnection();
+    		String sqlQuery = "SELECT Room.RoomName, Room.Capacity"
+    				+ "FROM Room"
+    				+"LEFT JOIN (SELECT m.EventID, m.RoomName, e.EventName, e.From, e.To"
+    				+"FROM Meeting AS m"
+    				+"NATURAL JOIN (SELECT *"
+    				+"FROM Event"
+    				+"WHERE (`From` < ? AND `To` < ?) OR (`From` > ?)) AS e) "
+    				+"AS me ON Room.RoomName = me.RoomName"
+    				+"WHERE (`Capacity` > ?  AND (`EventID` != ? OR `EventID` is NULL))";
+    		PreparedStatement preparedStatement = con.prepareStatement(sqlQuery);
+    		preparedStatement.setString(1, event.getFrom().toString());
+    		preparedStatement.setString(2, event.getFrom().toString());
+    		preparedStatement.setString(3, event.getTo().toString());
+    		preparedStatement.setInt(4, event.getParticipants().size());
+    		preparedStatement.setInt(5, event.getEventID());
+    		ResultSet resultSet = preparedStatement.executeQuery();
+    		while(resultSet.next()){
+    			String roomName = resultSet.getString("RoomName");
+    			int roomCapacity = resultSet.getInt("Capacity");
+    			availableRooms.add(new Room(roomName, roomCapacity));
+    		}
+    		resultSet.close();
+    		preparedStatement.close();
+    		con.close();
+    		return availableRooms;
+    	}
+    	catch(SQLException e){
+    		System.out.println(e);
+    		return null;
+    	}
     }
+    public static void main(String[] args) {
+//        ArrayList<Room> rooms = RoomQueries.getRooms();
+//        System.out.println("Printing a list of the room entries in the database table ROOM:");
+//        for (Room room : rooms) {
+//            System.out.println(room);
+//        }
+    	
 }
+}
+
