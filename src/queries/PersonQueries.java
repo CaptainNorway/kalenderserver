77edb0a2b3125e93411378f1c3eb00fec3a157b6
Sondre <sondrehjetland@gmail.com>
2015-02-26 14:27:46 +0100

    Javadoc

--- src/queries/PersonQueries.java
+++ src/queries/PersonQueries.java
@@ -8,6 +8,7 @@ import models.Person;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,21 +17,19 @@ import java.util.Scanner;
  */
 public class PersonQueries {
 
-
-
-
     /**
-     * @param username
+     * Retrieves information about a user, provided the username
+     *
+     * @param username - username of a user
+     * @return Person
      */
     public static Person getPerson(String username) {
 
-        ArrayList<Person> person = new ArrayList<>();
         String pass;
         Connection con = DBConnect.getConnection();
         //Execute query
         try {
-            String sql = "SELECT * FROM Person " +
-                    "WHERE Username = ?;";
+            String sql = "SELECT * FROM Person WHERE Username = ?;";
 
             PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setString(1, username);
@@ -50,19 +49,67 @@ public class PersonQueries {
         }
     }
 
+    /**
+     * Creates a person given a Person-object.
+     *
+     * @param person - Format: username, password, name
+     */
+    public static void createPerson(Person person) {
+
+        String pass;
+        Connection con;
+        PreparedStatement prep;
+        //Execute query
+        try {
+
+            con = DBConnect.getConnection();
+            con.setAutoCommit(false);
+
+            String query = "INSERT INTO Person (Username, Password, Name) VALUES (?, ?, ?);";
+            prep = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            prep.setString(1, person.getUsername());
+            prep.setString(2, person.getPassword());
+            prep.setString(3, person.getName());
+            prep.executeUpdate();
+            ResultSet keys = prep.getGeneratedKeys();
+            keys.next();
+            int key = keys.getInt(1);
+
+            query = "INSERT INTO UserGroup(GroupName) VALUES (?);";
+            prep = con.prepareStatement(query);
+            prep.setString(1, person.getName());
+            prep.executeUpdate();
+
+            query = "INSERT INTO PersonUserGroup (PersonID, UserGroupID) VALUES(?, LAST_INSERT_ID())";
+            prep = con.prepareStatement(query);
+            prep.setInt(1, key);
+            prep.executeUpdate();
+
+            con.commit();
+            prep.close();
+            con.close();
+            System.out.println(person + " was created");
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Retrieves the password of a given user.
+     *
+     * @param username - Username of a user.
+     * @return String
+     */
     public static String getPassword(String username) {
 
         String pass;
         Connection con = DBConnect.getConnection();
         //Execute query
         try {
-            String sql = "SELECT Password FROM Person " +
-                    "WHERE Username = ?;";
-
+            String sql = "SELECT Password FROM Person WHERE Username = ?;";
             PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setString(1, username);
             ResultSet rs = pstmt.executeQuery();
-
             rs.next();
             pass = rs.getString("Password");
 
@@ -76,6 +123,14 @@ public class PersonQueries {
         }
     }
 
+    /**
+     * Authenticate a user provided the username & password.
+     * Uses {@link #getPassword(String) getPassword} method
+     *
+     * @param username
+     * @param pass
+     * @return boolean - authenticate True/False
+     */
     public static boolean authenticate(String username, String pass) {
 
         try {
@@ -94,6 +149,7 @@ public class PersonQueries {
     }
 
     public static void main(String[] agrs) {
+        //createPerson(new Person("Sondrehh", "1234", "Sondre Hjetland"));
         Scanner user_input = new Scanner(System.in);
         System.out.println("Username: ");
         String username = user_input.next();
