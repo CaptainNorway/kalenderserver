e812d862e530cd044d51ad62a54d3e873cb25a42
CPekko <eirik_fosse2@hotmail.com>
2015-03-06 10:40:26 +0100

    Sewing together stuff that shall happen when an event is created.

--- src/queries/NotificationQueries.java
+++ src/queries/NotificationQueries.java
@@ -62,7 +62,7 @@ public static ArrayList<Notification> getNotifications(Person person){
 			notifications.add(new Notification(rs.getInt("NoteID"), rs.getString("Note"), 
 			new UserGroup(rs.getInt("UserGroupID"), rs.getString("GroupName"), null), 
 			null, new Event(rs.getInt("EventID"), rs.getString("EventName"), rs.getString("EventNote"), null, 
-			LocalDateTime.parse(rs.getTimestamp("From").toString(), formatter), LocalDateTime.parse(rs.getTimestamp("To").toString(), formatter), null)));
+			LocalDateTime.parse(rs.getTimestamp("From").toString(), formatter), LocalDateTime.parse(rs.getTimestamp("To").toString(), formatter), null), rs.getInt("IsInvite")));
 		}
 		rs.close();
 		prep.close();
